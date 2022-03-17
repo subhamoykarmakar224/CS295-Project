743d3956b245e607c9d38db416d0a5e03997375c
farquad <farquad@desktoppopos.lan>
2022-03-17 12:38:57 -0700

    new gdprbench grapsh

--- code/appgdpr/dbReset.sh
+++ code/appgdpr/dbReset.sh
@@ -3,9 +3,9 @@
 
 # Exit on first error
 set -e
-sudo -u postgres psql -c 'drop database IF EXISTS sieve'
-sudo -u postgres psql -c 'create database sieve'
-sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE sieve TO sieve'
+sudo -u postgres psql -c 'drop database IF EXISTS the_db'
+sudo -u postgres psql -c 'create database the_db'
+#sudo -u postgres psql -c 'GRANT ALL PRIVILEGES ON DATABASE sieve TO sieve'
 #change directory to howevermany entries we want to test after
-sudo -u postgres psql -d sieve -c '\i /home/farquad/Documents/gdprsearch/sieve/data/mall_data50kSieveComplete.sql'
+sudo -u postgres psql -d the_db -c '\i /home/farquad/Documents/gdprsearch/sieve/data/mall_data50kbase.sql'
 sudo service postgresql restart
